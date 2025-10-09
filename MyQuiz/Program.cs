5ba15278b7e769a3cdb76b3743ae5003b4da4440
simonik92 <caltea.simona@yahoo.com>
2025-10-09 16:20:58 +0300

    test the method

--- MyQuiz/Program.cs
+++ MyQuiz/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Question[] questions = new Question[]
+            {
+                new Question(
+                            "What is the capital of France?", // Question text
+                            new string[] { "Berlin", "Madrid", "Paris", "Rome" }, // Possible answers
+                            2 // Index of the correct answer (0-based index
+                            )
+            };
+
+            Quiz quiz = new Quiz(questions);
+            quiz.DisplayQuestion(questions[0]);
+            Console.ReadLine();
         }
     }
 }
